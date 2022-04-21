e6f0053519a16b6fddf4dd5e80e9bae9f03d6084
shivrajbande <shivabande09@gmail.com>
2022-04-21 17:56:02 +0530

    added some details

--- api/User.js
+++ api/User.js
@@ -6,57 +6,40 @@ const User = require('./../models/User');
 const router = express.Router();
 
 const users = new User({
-    name:'19BD1A0508',
+  name: '19BD1A0508',
 })
 users.save();
 
 
 // //getting student data
-router.post("/Search",(req,res)=>{
+router.post("/Search", (req, res) => {
 
-    // console.log(req.body.name);
+  // console.log(req.body.name);
 
-//     // seraching the data of the student in database
-//     User.find({name:req.body.name}).then(
-//         result =>{
+  //     // seraching the data of the student in database
+  User.find({ name: req.body.data },(error,result)=>{
 
-//             if(result.length)
-//             {
-//                 //add api to convert data into qr code
+    if(error)
+    {
+        res.json({
+                      Value:0,
+                      status: "FAILED TO FIND STUDENT",
+                      message: "An error occured!!"
+                  })
+    }
 
-//                 console.log(result);
-                  
+     else {
+      
 
+      res.json({
 
-           
-
-// qr.toDataURL(url, (err, src) => {
-//     if (err) res.send("Error occured");
-  
-//     // Let us return the QR code image as our response and set it to be the source used in the webpage
-//     res.render("scan", { src });
-// });
-
+        status: "Success",
+        Value:1,
 
 
-//             }
 
-//         }
-//     ).catch( (err)=>{
-
-//         console.log(err);
-//         res.json({
-//             status: "FAILED TO FIND STUDENT",
-//             message: "An error occured!!"
-//         })
-
-//     })
-
-   res.json({
-
-    names:req.body.data,
-
-
-   })
-})
-module.exports = router;
+      })
+    }
+  })
+      });
+  module.exports = router;
