222902a251ad155e43987f2a3e2f7bcf8c78c687
shivrajbande <shivabande09@gmail.com>
2022-04-23 13:41:24 +0530

    Modified Login.js

--- api/Login.js
+++ api/Login.js
@@ -1,6 +1,7 @@
 const Details = require('./../models/Login');
 const express = require('express');
 const { rawListeners } = require('./Push');
+const User = require('../models/User');
 
 const router = express.Router();
 
@@ -16,18 +17,27 @@ router.get("/Login",(req,res)=>{
 
 router.post("/Login",(req,res)=>{
    
-    
-    const login = new Details({
-         Name : req.body.name,
-         Password : req.body.password,
+    Details.find({Name : req.body.name})
+    .then( (user) => {
+        return res.status(400).send("User already exists");
     })
+    .then( ()=>{
 
-    login.save();
-    res.json({
-        Status:"Data Added to the DB!!",
-        data:req.body.password,
-        
+        const login = new Details({
+            Name : req.body.name,
+            Password : req.body.password,
+       })
+   
+       login.save();
+
     })
+    .then(result => {
+        return res.status(200).send(result);
+      })
+    .catch(error => {
+        console.log("Error Adding new User", error);
+        res.status(500).send("Error");
+      });
 })
 module.exports = router;
 
