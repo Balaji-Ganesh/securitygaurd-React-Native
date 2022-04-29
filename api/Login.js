3bad375e655997672fc9738581c1283c999c719f
shivrajbande <shivabande09@gmail.com>
2022-04-29 08:49:07 +0530

    made some minor changes

--- api/Login.js
+++ api/Login.js
@@ -30,6 +30,62 @@ router.post("/Login",async(req,res)=>{
         
     })
 })
+
+router.get("/ChangePassword", (res, res) => {
+  res.send("hii");
+});
+
+router.post("/ChangePassword", async (req, res) => {
+  // const Tname = req.body.name;
+
+  // const UserName = req.body.teachername;
+  // const oldPassword = req.body.oldpassword;
+  // const newpassword = req.body.newpassword;
+
+  // var query = { Name : UserName,Password : oldPassword};
+
+  res.json({
+    data: "data got it!!",
+  });
+
+  // await Details.updateOne(
+
+  //     query,
+
+  //   { Password: newpassword },
+  //   (error,result)=>{
+
+  //     if(error){
+  //       res.json({
+
+  //         status:400,
+  //         Data:"error",
+  //       })
+  //     }
+
+  //     if(result!=null){
+
+  //       res.json({
+
+  //        status : 200,
+  //        data:"Updated successsfully!!"
+
+  //       })
+  //     }
+
+  //     else
+  //    {
+  //      res.json({
+  //        status: 400,
+  //        Data: "Unable to update data",
+  //      });
+  //    }
+
+  //   }
+
+  // );
+});
+
 module.exports = router;
 
 
