c0aa682e90aa80d4465e62925974b5f1b6e4f4e0
shivrajbande <shivabande09@gmail.com>
2022-04-29 10:56:21 +0530

    added change password feature

--- api/Login1.js
+++ api/Login1.js
@@ -16,6 +16,7 @@ router.post("/Login1", async (req, res) => {
   {
     const newpassword = req.body.newpassword;
    const k = await Findteacher.updateOne({ $set: { Password: newpassword } });
+   k.save();
 
 
   }
