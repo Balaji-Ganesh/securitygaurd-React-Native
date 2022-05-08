75d335131175daba5aaaf175f4558a1d79680137
shivrajbande <shivabande09@gmail.com>
2022-05-08 11:04:58 +0530

    shjih

--- api/Login.js
+++ api/Login.js
@@ -1,12 +1,12 @@
+const Details = require('./../models/Login');
 const express = require('express');
 const bcrypt = require("bcrypt");
 
-const app = express();
-const Details = require("./../models/Login");
+const router = express.Router();
 
 
 
-app.get("/Login",(req,res)=>{
+router.get("/Login",(req,res)=>{
 
     res.json({
         name:"shiv",
@@ -16,46 +16,46 @@ app.get("/Login",(req,res)=>{
 
 
 
-app.post("/Login",async(req,res)=>{
+router.post("/Login",async(req,res)=>{
 
-//    const data = await Details.find({Name:req.body.name});
-//    console.log(data);
+   const data = await Details.find({Name:req.body.name});
+   console.log(data);
 //    const hashpassword = data[0].Password;
 //    const userpassword = req.body.password;
 
-    // const resu = await bcrypt.compare(userpassword, hashpassword);
+//     const resu = await bcrypt.compare(userpassword, hashpassword);
 
-    // if(data.length > 1)
-    // {
-    //    res.json({
-    //      Value: 1,
-    //      Status:"Details exist!",
-    //    }); 
+    if(data.length > 1)
+    {
+       res.json({
+         Value: 1,
+         Status:"Details exist!",
+       }); 
 
-    // }
-    // else{
-    //     res.json({
-    //         Value:0,
-    //         Status:"Teacher not exist!!",
-    //     })
-    // }
+    }
+    else{
+        res.json({
+            Value:0,
+            Status:"Teacher not exist!!",
+        })
+    }
 
 
    
-    const securePassword = await bcrypt.hash(req.body.password, 10);
-    const login = new Details({
-         Name : req.body.name,
-         Password : securePassword,
-    })
+    // const securePassword = await bcrypt.hash(req.body.password, 10);
+    // const login = new Details({
+    //      Name : req.body.name,
+    //      Password : securePassword,
+    // })
 
-    const result = await login.save();
-    res.json({
-        Status:"Data Added to the DB!!",
-        value:result,
+    // const result = await login.save();
+    // res.json({
+    //     Status:"Data Added to the DB!!",
+    //     value:result,
         
-    })
+    // })
 })
-module.exports = app;
+module.exports = router;
 
 
 
