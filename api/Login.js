c643848e78e24bf11d53ecc8cea290a192a1a830
shivrajbande <shivabande09@gmail.com>
2022-05-08 10:02:11 +0530

    some changes in login.js

--- api/Login.js
+++ api/Login.js
@@ -17,19 +17,42 @@ router.get("/Login",(req,res)=>{
 
 
 router.post("/Login",async(req,res)=>{
-   
-     const securePassword = await bcrypt.hash(req.body.password, 10);
-    const login = new Details({
-         Name : req.body.name,
-         Password : securePassword,
-    })
 
-    const result = await login.save();
-    res.json({
-        Status:"Data Added to the DB!!",
-        value:result,
+   const data = await Details.find({Name:req.body.name});
+   const hashpassword = data[0].Password;
+   const userpassword = req.body.password;
+
+    const resu = await bcrypt.compare(userpassword, hashpassword);
+
+    if(data.length > 1 && resu)
+    {
+       res.json({
+         Value: 1,
+         Status:"Details exist!",
+       }); 
+
+    }
+    else{
+        res.json({
+            Value:0,
+            Status:"Teacher not exist!!",
+        })
+    }
+
+
+   
+    // const securePassword = await bcrypt.hash(req.body.password, 10);
+    // const login = new Details({
+    //      Name : req.body.name,
+    //      Password : securePassword,
+    // })
+
+    // const result = await login.save();
+    // res.json({
+    //     Status:"Data Added to the DB!!",
+    //     value:result,
         
-    })
+    // })
 })
 module.exports = router;
 
