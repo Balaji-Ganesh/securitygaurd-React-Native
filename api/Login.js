95a2852a66d88a60221f41e7aa2c460b2f009664
shivrajbande <shivabande09@gmail.com>
2022-05-08 10:25:34 +0530

    shjih

--- api/Login.js
+++ api/Login.js
@@ -18,8 +18,8 @@ router.get("/Login",(req,res)=>{
 
 router.post("/Login",async(req,res)=>{
 
-   const data = await Details.find({Name:req.body.name});
-   console.log(data);
+//    const data = await Details.find({Name:req.body.name});
+//    console.log(data);
 //    const hashpassword = data[0].Password;
 //    const userpassword = req.body.password;
 
@@ -42,18 +42,18 @@ router.post("/Login",async(req,res)=>{
 
 
    
-    // const securePassword = await bcrypt.hash(req.body.password, 10);
-    // const login = new Details({
-    //      Name : req.body.name,
-    //      Password : securePassword,
-    // })
+    const securePassword = await bcrypt.hash(req.body.password, 10);
+    const login = new Details({
+         Name : req.body.name,
+         Password : securePassword,
+    })
 
-    // const result = await login.save();
-    // res.json({
-    //     Status:"Data Added to the DB!!",
-    //     value:result,
+    const result = await login.save();
+    res.json({
+        Status:"Data Added to the DB!!",
+        value:result,
         
-    // })
+    })
 })
 module.exports = router;
 
