50f591f6505dc14ec79be703db0ae5792c6f57be
Balaji-Ganesh <balganeshboda27@gmail.com>
2022-05-09 11:46:12 +0530

    Resolved permission retrival for GateKeeper

--- api/User.js
+++ api/User.js
@@ -1,27 +1,41 @@
 const express = require("express");
-
-// const app = express();
 const User = require("./../models/User");
-
 const router = express.Router();
 
-// const users = new User({
-//   RollNumber : '19BD1A0508',
-//   // Type:true,
-// })
-// users.save();
-
-// //getting SINGLE student data
+// //getting student data
 router.post("/Search", async (req, res) => {
   // console.log(req.body.name);
 
-  // seraching the data of the student in database
+  //     // seraching the data of the student in database
   const result = await User.find({ RollNumber: req.body.data });
 
   if (result.length > 0) {
+    res.json({
+      status: "Success",
+      Value: 1,
+      // data : req.body.data
+    });
+  } else {
+    res.json({
+      Value: 0,
+      status: "FAILED TO FIND STUDENT",
+      message: "An error occured!!",
+    });
+  }
+});
+
+// //getting SINGLE student data <-- gate keeper requests on this route..
+router.post("/Validate/:rollNo", async (req, res) => {
+  // console.log(req.body.name);
+
+  // seraching the data of the student in database
+  const result = await User.find({ RollNumber: req.params.rollNo });
+
+  if (result.length > 0) {
+    // console.log(result);
     // Update the "Type", after scanned (for GatePass only)
-    if (result.Type == 1) {
-      // 1: GatePass, 0: LunhcPass, -1: AlreadyScanned/Expired
+    if (result[0].Type == 1) {
+      // 1: GatePass, 0: LunchPass, -1: AlreadyScanned/Expired
       const filter = { RollNumber: req.body.data };
       const update = { Type: -1 }; // -1 to hold the expiration.
 
@@ -31,15 +45,22 @@ router.post("/Search", async (req, res) => {
         "[INFO] Changed expiration of permission (1 -> -1) Successfully"
       );
     }
-    res.json({
-      status: "Success",
-      Value: 1,
+    console.log(
+      "[INFO] Permission retrieved successfully for RollNumber: " +
+        req.params.rollNo
+    );
+    res.status(200).json({
+      RollNumber: result[0].RollNumber,
+      facultyId: result[0].Name,
+      Type: result[0].Type,
     });
   } else {
-    res.json({
-      Value: 0,
-      status: "FAILED TO FIND STUDENT",
-      message: "An error occured!!",
+    console.log(
+      "[ERROR] Permission retrieval failed for RollNumber: " + req.params.rollNo
+    );
+    res.status(404).json({
+      message:
+        "No such permission found with that RollNumber: " + req.params.rollNo,
     });
   }
 });
