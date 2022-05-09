1ab4c0dee37d779a1555a6392b99b3205a164768
Balaji-Ganesh <balganeshboda27@gmail.com>
2022-05-09 10:37:49 +0530

    Added feature of admin adding a new user

---
+++ api/Details.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const Details = require("./../models/Login");
+const bcrypt = require("bcrypt");
+
+// Creating a new user..
+// Registration.. <- will happen through postman for creation of new users.
+router.post("/", async (request, response) => {
+  try {
+    /*---------------------------------- Registering a new user--------------------------------- */
+    // Generate the hashed password -- Security concern..
+    const saltRounds = 10; // Tune it according, as per requirement.
+    console.log(request.body);
+    const defaultPwd = "GatePassApplication";
+    const hashedPwd = await bcrypt.hash(defaultPwd, saltRounds);
+    // Go for creation of the new user..
+    const newUser = new Details({
+      id: request.body.id,
+      password: hashedPwd,
+      name: request.body.name,
+      role: request.body.role,
+      profilePicture: "", // currently no use. May be in future.
+    });
+
+    // Save the new user..
+    const user = await newUser.save();
+    // After successful save..!!
+    // Remove the password field before sending..
+    const userModified = Object.keys(user).reduce((object, key) => {
+      if (key !== "password") {
+        object[key] = user[key];
+      }
+      return object;
+    }, {});
+    response.status(200).json(userModified);
+    console.log(
+      "[SUCCESS] New user has with default password is successfully Created. User is requested to change in first login."
+    );
+  } catch (error) {
+    response.status(500).json(error);
+    console.log("[ERROR] New user Registration failed. Desc: " + error);
+  }
+});
+
