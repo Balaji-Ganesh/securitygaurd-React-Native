99b8e2450c8f37ba8adb4234049b93da8ddf9a03
Balaji-Ganesh <balganeshboda27@gmail.com>
2022-05-09 09:54:02 +0530

    Tweaks to Login.js(details)

--- models/Login.js
+++ models/Login.js
@@ -2,10 +2,26 @@
 
 const mongoose = require('mongoose');
 
-const TeacherDetails = mongoose.Schema({
-    Name:String,
-    Password:String,
-})
+const TeacherDetails = mongoose.Schema(
+  {
+    Name: {
+      type: String,
+      required: true,
+    },
+    Password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: {
+        values: ["Student", "Teacher", "GateKeeper"], // Only these values are allowed
+        message: "{VALUE} is not supported", // Mongoose replaces {VALUE} with the value being validated.
+      },
+    },
+  },
+  { timestamps: true }
+);
 
 const Details = mongoose.model('Details',TeacherDetails);
 
