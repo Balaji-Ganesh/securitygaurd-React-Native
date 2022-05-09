cbd897fed75637363c6248c93e936b539b2a01e0
Balaji-Ganesh <balganeshboda27@gmail.com>
2022-05-09 10:12:39 +0530

    Added route to retrieve all permissions (for dashboard)

--- api/User.js
+++ api/User.js
@@ -1,12 +1,9 @@
-
-const express = require('express');
-
+const express = require("express");
 
 // const app = express();
-const User = require('./../models/User');
+const User = require("./../models/User");
 
 const router = express.Router();
-
 
 // const users = new User({
 //   RollNumber : '19BD1A0508',
@@ -14,39 +11,41 @@ const router = express.Router();
 // })
 // users.save();
 
-
-
-
-// //getting student data
-router.post("/Search", async(req, res) => {
-
+// //getting SINGLE student data
+router.post("/Search", async (req, res) => {
   // console.log(req.body.name);
 
   //     // seraching the data of the student in database
   const result = await User.find({ RollNumber: req.body.data });
 
-  if(result.length > 0)
-  {
-
+  if (result.length > 0) {
     res.json({
       status: "Success",
-      Value:1,
+      Value: 1,
       // data : req.body.data
-    })
-
-  }
-  else
-  {
-
+    });
+  } else {
     res.json({
-      Value:0,
+      Value: 0,
       status: "FAILED TO FIND STUDENT",
-      message: "An error occured!!"
-  })
-
+      message: "An error occured!!",
+    });
   }
+});
 
-    
+// Get all the permissions.. -- for ADMIN.
+router.get("/", async (request, response) => {
+  let posts;
+  try {
+    // Get all the posts..
+    posts = await Permission.find();
 
-      });
-  module.exports = router;
+    // Send the retrieved (filtered) posts..
+    console.info("[SUCCESS] Multiple permissions retrieved successfully");
+    response.status(200).json(posts);
+  } catch (error) {
+    console.error("[ERROR] Error in retrieving all permissions.");
+    response.status(500).json("Sorry, Unable to retrieve permissions.");
+  }
+});
+module.exports = router;
