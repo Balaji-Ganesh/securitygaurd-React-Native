1afc7a0c8d027df14948d0b2c48603c7b16445e4
shivrajbande <shivabande09@gmail.com>
2022-05-09 13:14:25 +0530

    changed login.js

--- api/Login.js
+++ api/Login.js
@@ -13,18 +13,7 @@ router.get("/Login", (req, res) => {
 });
 
 // For creating a new user
-router.post("/Login", async (req, res) => {
-  const login = new Details({
-    Name: req.body.name,
-    Password: req.body.password,
-  });
 
-  const result = await login.save();
-  res.json({
-    Status: "Data Added to the DB!!",
-    value: result.name,
-  });
-});
 
 // for getting a existing user...(for authentication purpose) based on
 router.post("/api/authenticate", async (request, response) => {
